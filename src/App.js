b94606c353c8cdae4a7bc5bb0332c28f72a58d89
Santiago Arocha <arochas846@gmail.com>
2023-03-02 17:55:42 -0300

    Setting components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { ShoppingList } from './components/styles/list';
 
 function App() {
   return (
     <div className="App">
       <h1>Tic tac Toe</h1>
+      <ShoppingList name = 'SantiHouse'></ShoppingList>
     </div>
   );
 }
