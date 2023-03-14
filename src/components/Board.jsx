ceb978c4a28266417e23e8ef904d20c962d826bf
Santiago Arocha <arochas846@gmail.com>
2023-03-14 17:55:24 -0300

    Changing btw players turns

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,16 +2,55 @@ import React, { useState } from "react";
 import { Square } from "./Square.jsx";
 import './styles/board.css';
 
+
+const calculateWinner = (squares) => {
+
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    for (let index = 0; index < lines.length; index++) {
+        const [a,b,c] = lines[index];
+        
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        } 
+    }
+
+    return null;
+}
+
 export function Board () {
 
+    
+    const [xIsNext, setxIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     const handleClick = (i) => {
-        const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
-        setSquares(nextSquares);
-    } 
+        //if the square i position is true, it is filled
+        if (squares[i] || calculateWinner[squares]) {
+            return;
+        }
 
+        const nextSquares = squares.slice();
+
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
+
+        setSquares(nextSquares);
+        setxIsNext(!xIsNext);
+
+    } 
 
         return(
             <>
@@ -38,3 +77,4 @@ export function Board () {
         );
     
 }
+
